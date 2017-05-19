e10ce1aa7ed74561f63731160319b466bdb4a468
ralphminor <ralphminor@users.noreply.github.com>
2017-05-19 16:43:12 -0600

    Created app.js.  Defined and referenced module, defined and inserted listItem component, defined controller and template.  Referenced app.js in index.html and connected the module to the page.

---
+++ app/client/javascripts/app.js
@@ -0,0 +1,14 @@
+(function() {
+  'use strict'
+    angular.module("app", [])
+
+      .component('itemList', {
+
+        controller: function () {
+          const vm = this
+        },
+        template: `
+          <h4>List will go here...</h4>
+        `
+      })
+}())
