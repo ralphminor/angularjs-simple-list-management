ac1023f18c8fcbd45bcbd906008d754331576ce4
ralphminor <ralphminor@users.noreply.github.com>
2017-05-19 18:36:27 -0600

    Completed form and functions to add and delete list items.

--- app/client/javascripts/app.js
+++ app/client/javascripts/app.js
@@ -6,9 +6,60 @@
 
         controller: function () {
           const vm = this
+
+          vm.$onInit = onInit
+          vm.addItem = addItem
+          vm.deleteItem = deleteItem
+
+          function onInit() {
+            vm.items=[
+              {name:"boat", quantity:1},
+              {name:"car", quantity:2},
+              {name:"hammer", quantity:1},
+              {name:"phone", quantity:1},
+            ];
+          }
+
+          function addItem() {
+            if (vm.item) {
+              vm.items.push(vm.item);
+              delete vm.item;
+            }
+          }
+
+          function deleteItem(e, item) {
+            e.preventDefault()
+            vm.items.splice(vm.items.indexOf(item), 1)
+          }
+
         },
         template: `
-          <h4>List will go here...</h4>
+          <form ng-submit="$ctrl.addItem()">
+            <p>
+              <label for="name">Name</label>
+              <input type="text" id="name" ng-model="$ctrl.item.name"/>
+            </p>
+            <p>
+              <label for="quantity">Quantity</label>
+              <input type="text" id="quantity" ng-model="$ctrl.item.quantity"/>
+            </p>
+            <p>
+              <button type="submit">Add Item</button>
+            </p>
+          </form>
+
+          <div class="row">
+            <div class="col-sm-1">Name</div>
+            <div class="text-center col-sm-1">Quantity</div>
+            <div class="text-center col-sm-1">Delete?</div>
+          </div>
+          <div ng-repeat="item in $ctrl.items">
+            <div class="row">
+              <div class="col-sm-1">{{$index + 1}}. {{ item.name }}</div>
+              <div class="text-center col-sm-1">{{ item.quantity}}</div>
+              <div class="text-center col-sm-1"><a href="#" ng-click="$ctrl.deleteItem($event, item)">❌</a></div>
+            </div>
+          </div>
         `
       })
 }())
